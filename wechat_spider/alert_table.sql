24444c2ca56d3fa3d1fb3cc17775ef0f28020123
liubo <564773807@qq.com>
2019-06-17 10:57:03 +0800

    fixed sql bug

--- wechat_spider/alert_table.sql
+++ wechat_spider/alert_table.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `wechat_article_task` ADD COLUMN `__biz` varchar(50)
-ALTER TABLE `wechat_article_dynamic_table` ADD COLUMN `__biz` varchar(50)
-ALTER TABLE `wechat_article_comment_table` ADD COLUMN `__biz` varchar(50)
+ALTER TABLE `wechat_article_dynamic` ADD COLUMN `__biz` varchar(50)
+ALTER TABLE `wechat_article_comment` ADD COLUMN `__biz` varchar(50)

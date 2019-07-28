5e973e9453c05a7d3b3b5b0c28ce9ca3d19382f7
liubo <564773807@qq.com>
2019-07-29 00:21:07 +0800

    添加抓取时间范围配置、文章表添加url字段、修复bug

--- wechat_spider/alert_table.sql
+++ wechat_spider/alert_table.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `wechat_article_task` ADD COLUMN `__biz` varchar(50)
 ALTER TABLE `wechat_article_dynamic` ADD COLUMN `__biz` varchar(50)
 ALTER TABLE `wechat_article_comment` ADD COLUMN `__biz` varchar(50)
+ALTER TABLE `wechat_article` ADD COLUMN `url` varchar(255)
+
